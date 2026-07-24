7eed8172074841cbda8b3b8263cc469a79766e10
FelipePamplon <118272865+FelipePamplon@users.noreply.github.com>
2026-07-24 02:25:54 +0000

    Fix nested app folder structure

---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+export default function Home(){
+
+return(
+
+<main
+style={{
+background:"#111111",
+height:"100vh",
+display:"flex",
+justifyContent:"center",
+alignItems:"center",
+color:"white",
+fontFamily:"sans-serif"
+}}
+>
+
+<div>
+
+<h1
+style={{
+fontSize:48,
+fontWeight:700,
+marginBottom:10
+}}
+>
+
+VX Capital
+
+</h1>
+
+<p>
+
+Sistema Financeiro
+
+</p>
+
+</div>
+
+</main>
+
+)
+
+}
